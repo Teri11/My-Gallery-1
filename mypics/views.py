ed5bdee6c55ea0e490181390dfd31e17dbf37fe2
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-03 10:09:22 +0300

    Add a view function to get and render photos template

--- mypics/views.py
+++ mypics/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
+from .models import Photo
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def home(request):
   return render(request, 'index.html')
+
+def photos(request):
+  photos =Photo.objects.all().order_by("-posted_at")
+  return render(request,'photos.html',{'photos':photos})
