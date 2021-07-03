5ba761a582354b79c1471745c1fc653334f9b43b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-03 17:33:36 +0300

    Update photo class methods

--- mypics/models.py
+++ mypics/models.py
@@ -24,8 +24,8 @@ class Photo(models.Model):
   owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
   posted_at = models.DateTimeField(auto_now_add=True)
   image = CloudinaryField('image')
-  location = models.ForeignKey('Location')
-  category = models.ForeignKey('Category')
+  location = models.ForeignKey('Location',on_delete=models.CASCADE)
+  category = models.ForeignKey('Category',on_delete=models.CASCADE)
 
   def save_photo(self):
     self.save()
@@ -38,6 +38,24 @@ class Photo(models.Model):
     update = cls.objects.filter(id = id).update(title = title,description = description,owner=owner,image=image,location = location,category = category)
     return update
 
+  @classmethod
+  def get_photo_by_id(cls,id):
+    photo = cls.objects.filter(id= id).all()
+    return photo
+
+  @classmethod
+  def search_photo_by_category(cls,category):
+    photos = Photo.objects.filter(category__name__icontains=category)
+    return photos
+
+  @classmethod
+  def filter_photo_by_location(cls, location):
+    location = cls.objects.filter(location__id=location)
+    return location
+
+  class Meta:
+      ordering = ['name']
+      
   def __str__(self):
     return self.title
 
