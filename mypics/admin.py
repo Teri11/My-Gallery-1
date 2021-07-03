b2e96ad2dcefa749d81e86b0f61cb3c2334eb2b9
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-03 20:00:35 +0300

    Add Location and Category to admin site

--- mypics/admin.py
+++ mypics/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Owner, Photo
+from .models import Category, Location, Owner, Photo
 
 # Register your models here.
 admin.site.register(Owner)
 admin.site.register(Photo)
+admin.site.register(Location)
+admin.site.register(Category)
