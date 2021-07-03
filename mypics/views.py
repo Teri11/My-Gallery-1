87d96989b081cca4e58112eb84d522dd3d72e5e5
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-03 10:04:30 +0300

    Add a view function for index page

--- mypics/views.py
+++ mypics/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse,Http404
+import datetime as dt
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def home(request):
+  return render(request, 'index.html')
