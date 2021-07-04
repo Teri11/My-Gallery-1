67b607517500f47b6155000f903d8a95604b70eb
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-05 01:26:58 +0300

    Update pages

--- mypics/static/js/script.js
+++ mypics/static/js/script.js
@@ -2,21 +2,13 @@
 function imgWindow() {
   window.open("image")
 }
-
-const copy   = document.getElementById("copyButton");
-const selection = window.getSelection();
-const range = document.createRange();
-const textToCopy = document.getElementById("copyUrl")
-
-copy.addEventListener('click', function(e) {
-    range.selectNodeContents(textToCopy);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    const successful = document.execCommand('copy');
-    if(successful){
-      alert('Phot URL Copied!') ;
-    } else {
-      alert('Unable to copy!');  
-    }
-    window.getSelection().removeAllRanges()
+$(document).ready(function () {
+  $(function () {
+    $('.photoUrl').click(function () {
+      $(this).focus();
+      $(this).select();
+      document.execCommand('copy');
+      alert("Copied to clipboard");
+    });
+  });
 });
