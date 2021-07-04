1766bb83d311e1b2871b2564da7e19834f52f986
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-04 20:24:58 +0300

    Copy photo URL works

--- mypics/static/js/script.js
+++ mypics/static/js/script.js
@@ -1,3 +1,22 @@
 
- function imgWindow() {
-  window.open("image") }
+function imgWindow() {
+  window.open("image")
+}
+
+const copy   = document.getElementById("copyButton");
+const selection = window.getSelection();
+const range = document.createRange();
+const textToCopy = document.getElementById("copyUrl")
+
+copy.addEventListener('click', function(e) {
+    range.selectNodeContents(textToCopy);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    const successful = document.execCommand('copy');
+    if(successful){
+      alert('Phot URL Copied!') ;
+    } else {
+      alert('Unable to copy!');  
+    }
+    window.getSelection().removeAllRanges()
+});
