2154dc9d8cc25c481bf14500fb869b41776bddb2
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-05 16:40:07 +0300

    Add Location test class

--- mypics/tests.py
+++ mypics/tests.py
@@ -29,3 +29,28 @@ class CategoryTestClass(TestCase):
         category = Category.get_category_id(self.category.id)
         self.assertTrue(category.title == 'coding')
 
+class LocationTestCLass(TestCase):
+    #Set up Method
+    def setUp(self):
+        self.location = Location(title="Home")
+        self.location.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        self.location.save_location()
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+    def test_update(self):
+        location = Location.get_location_id(self.location.id)
+        location.update_location('Moringa')
+        location = Location.get_location_id(self.location.id)
+        self.assertTrue(location.title == 'Moringa')
